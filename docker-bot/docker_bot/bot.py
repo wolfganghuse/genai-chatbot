5ef12801d331f82f7ca81ac29818fe7bf7697f89
wolfganghuse <wolfgang.huse@nutanix.com>
2025-03-27 23:56:46 +0100

    adding timestamp

--- docker-bot/docker_bot/bot.py
+++ docker-bot/docker_bot/bot.py
@@ -1,9 +1,12 @@
 import os
-
 import streamlit as st
 from dotenv import load_dotenv
 from openai import OpenAI
-from pinecone import Pinecone, ServerlessSpec
+
+from sqlalchemy import create_engine, Column, String, Float, select
+from pgvector.sqlalchemy import Vector
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from streamlit.logger import get_logger
 
 load_dotenv(".env")
@@ -12,79 +15,85 @@ logger = get_logger(__name__)
 
 client = OpenAI(api_key=os.getenv("OPENAI_TOKEN"))
 
+# Database Setup
+db_url = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/dbname")
+engine = create_engine(db_url)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
-@st.cache_resource
-def load_pinecone(index_name="docker-genai"):
-    # initialize pinecone
-    pc = Pinecone(api_key=os.getenv("PINECONE_TOKEN"))
 
-    if index_name not in pc.list_indexes().names():
-        logger.info(f"Creating index {index_name}, therefore there are no videos yet.")
-        pc.create_index(
-            name=index_name,
-            dimension=1536,
-            metric="cosine",
-            spec=ServerlessSpec(cloud="aws", region="us-west-2"),
+class VideoEmbedding(Base):
+    __tablename__ = "video_embeddings"
+
+    id = Column(String, primary_key=True)
+    embedding = Column(Vector(1536))
+    initial_time = Column(Float)
+    title = Column(String)
+    thumbnail = Column(String)
+    video_url = Column(String)
+    text = Column(String)
+
+Base.metadata.create_all(bind=engine)
+    
+def query_pgvector(input_text):
+    session = SessionLocal()
+    try:
+        # Generate embedding for the input text
+        embedding_response = client.embeddings.create(
+            input=[input_text], model="text-embedding-3-small"
         )
-    index = pc.Index(index_name)
-    return index
+        input_embedding = embedding_response.data[0].embedding
+        
+        # Query the database for the most similar embeddings
+        #id, title, text, video_url
+        query = (
+            select(VideoEmbedding.id, VideoEmbedding.title, VideoEmbedding.text, VideoEmbedding.video_url, VideoEmbedding.embedding.cosine_distance(input_embedding).label('distance'))
+            .order_by('distance')
+            .limit(5)
+        )
+        results = session.execute(query).fetchall()
+        return results
+    except Exception as e:
+        logger.error(f"Error querying pgvector: {e}")
+        return []
+    finally:
+        session.close()
 
 
 def generate_response(input_text):
-    question_embedding = client.embeddings.create(
-        input=[input_text], model="text-embedding-3-small"
-    )
-    num_embeddings = list(question_embedding.data[0].embedding)
-    # print(list(question_embedding.data[0].embedding))
-    res_contex = load_pinecone().query(
-        vector=num_embeddings,
-        top_k=5,
-        include_metadata=True,
-    )
-
-    matches = res_contex["matches"]
-
-    context = (
-        "The following are the top 5 videos transcription that match your query: \n"
-    )
+    results = query_pgvector(input_text)
+    context = "The following are the top 5 video transcriptions that match your query: \n"
     references = ""
-    for match in matches:
-        context += "Title: " + match.metadata["title"] + "\n"
-        context += "Transcription: " + match.metadata["text"] + "\n"
-        references += "\n - " + match.metadata["video_url"] + "\n"
+    
+    for result in results:
+        context += f"Title: {result.title}\n"
+        context += f"Transcription: {result.text}\n"
+        references += f"\n - {result.video_url}\n"
 
     primer = """You are Q&A bot. A highly intelligent system that answers 
-    user questions based on the information provided by videos transcriptions. You can use your inner knowledge,
+    user questions based on the information provided by video transcriptions. You can use your inner knowledge,
     but consider more with emphasis the information provided. Put emphasis on the transcriptions provided. If you see titles repeated, you can assume it is the same video.
     Provide samples of the transcriptions that are important to your query.
     """
-
+    
     chat_completion = client.chat.completions.create(
         messages=[
             {"role": "system", "content": primer},
-            {
-                "role": "user",
-                "content": context,
-            },
+            {"role": "user", "content": context},
             {"role": "user", "content": input_text},
         ],
         model="gpt-4-turbo-preview",
     )
     response = chat_completion.choices[0].message.content
-
-    # Add video references
+    
     response += "\n Click on the following for more information: " + references
-
+    
     return response
-
 
 logger = get_logger(__name__)
 
-
-# Streamlit UI
 styl = """
 <style>
-    /* not great support for :has yet (hello FireFox), but using it for now */
     .element-container:has([aria-label="Select RAG mode"]) {{
       position: fixed;
       bottom: 33px;
@@ -94,8 +103,6 @@ styl = """
     .stChatFloatingInputContainer {{
         bottom: 20px;
     }}
-
-    /* Generate ticket text area */
     textarea[aria-label="Description"] {{
         height: 200px;
     }}
@@ -103,49 +110,35 @@ styl = """
 """
 st.markdown(styl, unsafe_allow_html=True)
 
-
 def chat_input():
-    user_input = st.chat_input("What you want to know about your videos?")
-
+    user_input = st.chat_input("What do you want to know about your videos?")
+    
     if user_input:
         with st.chat_message("user"):
             st.write(user_input)
         with st.chat_message("assistant"):
             st.caption("Dockerbot")
-            # result = output_function(
-            #     {"question": user_input, "chat_history": []}, callbacks=[stream_handler]
-            # )["answer"]
             result = generate_response(user_input)
-            # result = "I am a bot. I am still learning."
-            output = result
             st.session_state["user_input"].append(user_input)
-            st.session_state["generated"].append(output)
+            st.session_state["generated"].append(result)
             st.rerun()
 
-
 def display_chat():
-    # Session state
     if "generated" not in st.session_state:
         st.session_state["generated"] = []
-
     if "user_input" not in st.session_state:
         st.session_state["user_input"] = []
-
+    
     if st.session_state["generated"]:
         size = len(st.session_state["generated"])
-        # Display only the last three exchanges
         for i in range(max(size - 3, 0), size):
             with st.chat_message("user"):
                 st.write(st.session_state["user_input"][i])
-
             with st.chat_message("assistant"):
-                # st.caption(f"RAG: {st.session_state['rag_mode'][i]}")
                 st.caption("Dockerbot")
                 st.write(st.session_state["generated"][i])
-
         with st.container():
             st.write("&nbsp;")
-
 
 display_chat()
 chat_input()
--- yt-whisper/yt_whisper/app.py
+++ yt-whisper/yt_whisper/app.py
@@ -1,23 +1,20 @@
 import os
-import tempfile
+import argparse
+import json
+import numpy as np
+import ffmpeg
 from pathlib import Path
-from tempfile import mkdtemp
-
-import streamlit as st
 from dotenv import load_dotenv
 from openai import OpenAI
-from pytube import YouTube
-from pytube.exceptions import RegexMatchError
-from streamlit.logger import get_logger
-from yt_whisper.vtt_utils import merge_webvtt_to_list
 from sqlalchemy import create_engine, Column, String, LargeBinary, Float
+from pgvector.sqlalchemy import Vector
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import numpy as np
-import json
+import tempfile
+from yt_whisper.vtt_utils import merge_webvtt_to_list
 
 load_dotenv(".env")
-logger = get_logger(__name__)
+
 client = OpenAI(api_key=os.getenv("OPENAI_TOKEN"))
 
 # Database Setup
@@ -26,11 +23,12 @@ engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+
 class VideoEmbedding(Base):
     __tablename__ = "video_embeddings"
 
     id = Column(String, primary_key=True)
-    embedding = Column(LargeBinary)
+    embedding = Column(Vector(1536))
     initial_time = Column(Float)
     title = Column(String)
     thumbnail = Column(String)
@@ -42,7 +40,7 @@ Base.metadata.create_all(bind=engine)
 def store_embedding(video_data):
     session = SessionLocal()
     for entry in video_data:
-        embedding_vector = np.array(entry["embedding"]).tobytes()
+        embedding_vector=entry["embedding"]
         record = VideoEmbedding(
             id=entry["id"],
             embedding=embedding_vector,
@@ -56,111 +54,61 @@ def store_embedding(video_data):
     session.commit()
     session.close()
 
-@st.cache_data
 def process_video(video_url: str) -> dict[str, str]:
-    try:
-        from pytube.innertube import _default_clients
-
-        _default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID"]
-
-        yt_handler = YouTube(video_url)
-    except RegexMatchError:
-        st.error("Please enter a valid YouTube URL", icon="🚨")
-        return {}
-
-    with st.spinner("Processing your video"):
-        logger.info(f"Processing video: {yt_handler.watch_url}")
-        with tempfile.TemporaryDirectory() as tmp_dir:
-            audio_stream = yt_handler.streams.filter(only_audio=True).first()
-            audio_file = audio_stream.download(tmp_dir)
-            file_stats = os.stat(audio_file)
+    
+    video_title = video_url.rsplit("/", 1)[0]
+    print(f"Processing video file: {video_title}")
+    
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        audio_file = os.path.join(tmp_dir, "audio.mp3")
+        ffmpeg.input(video_url).output(audio_file, format="mp3", acodec="libmp3lame").run()
+        
+        with open(audio_file, "rb") as audio_file:
+            whisper_transcript = client.audio.transcriptions.create(
+                model="whisper-1", file=audio_file, response_format="vtt"
+            )
+        
+        seconds_to_merge = 8
+        transcript = merge_webvtt_to_list(whisper_transcript, seconds_to_merge)
+        stride = 3
+        video_data = []
+        
+        for block in range(0, len(transcript), stride):
+            initial_time = transcript[block]["initial_time_in_seconds"]
+            text = " ".join([t["text"] for t in transcript[block:block + stride]]).replace("\n", " ")
+            id = f"video-t{initial_time}" #need to change this to a unique id
             
-            if file_stats.st_size > 24 * 1024 * 1024:
-                st.error("Please select a shorter video, OpenAI has a 25MB limit", icon="🚨")
-                return {}
-            
-            with open(audio_file, "rb") as audio_file:
-                whisper_transcript = client.audio.transcriptions.create(
-                    model="whisper-1", file=audio_file, response_format="vtt"
-                )
-            
-            seconds_to_merge = 8
-            transcript = merge_webvtt_to_list(whisper_transcript, seconds_to_merge)
-            stride = 3
-            video_data = []
-            
-            for block in range(0, len(transcript), stride):
-                initial_time = transcript[block]["initial_time_in_seconds"]
-                text = " ".join([t["text"] for t in transcript[block:block + stride]]).replace("\n", " ")
-                id = f"{yt_handler.video_id}-t{initial_time}"
-                
-                video_data.append(
-                    {
-                        "id": id,
-                        "initial_time": initial_time,
-                        "title": yt_handler.title,
-                        "thumbnail": yt_handler.thumbnail_url,
-                        "video_url": f"{video_url}&t={initial_time}s",
-                        "text": text,
-                        "embedding": client.embeddings.create(
-                            input=text, model="text-embedding-3-small"
-                        ).data[0].embedding,
-                    }
-                )
-                
-            store_embedding(video_data)
-            
-            output_transcript_path = Path(st.session_state.tempfolder) / f"{yt_handler.video_id}.txt"
-            with open(output_transcript_path, "w") as transcript_file:
-                transcript_text = "\n".join([t["text"] for t in transcript])
-                transcript_file.write(transcript_text)
-            
-            return {
-                "video_id": yt_handler.video_id,
-                "title": yt_handler.title,
-                "thumbnail": yt_handler.thumbnail_url,
-            }
+            video_data.append(
+                {
+                    "id": id,
+                    "initial_time": initial_time,
+                    "title": video_title,
+                    "thumbnail": "",  # No thumbnail for local files
+                    "video_url": f"{video_url}#t={initial_time}", 
+                    "text": text,
+                    "embedding": client.embeddings.create(
+                        input=text, model="text-embedding-3-small"
+                    ).data[0].embedding,
+                }
+            )
+        
+        store_embedding(video_data)
+        
+        output_transcript_path = Path("transcription.txt")
+        with open(output_transcript_path, "w") as transcript_file:
+            transcript_text = "\n".join([t["text"] for t in transcript])
+            transcript_file.write(transcript_text)
+        
+        return {"title": "Local Video"}
 
 def main():
-    logger.debug("Rendering app")
-    if "tempfolder" not in st.session_state:
-        st.session_state.tempfolder = Path(mkdtemp(prefix="yt_transcription_"))
-    if "videos" not in st.session_state:
-        st.session_state.videos = []
-    if "processing" not in st.session_state:
-        st.session_state.processing = False
-
-    st.header("Chat with your YouTube videos")
-    st.write(
-        "This app uses OpenAI's Whisper model to generate a transcription of your videos and store embeddings in PostgreSQL using pgvector."
-    )
-
-    yt_uri = st.text_input("YouTube URL", "https://www.youtube.com/watch?v=8CY2aq3tcXA")
-    if st.button(
-        "Submit",
-        type="primary",
-        on_click=lambda: setattr(st.session_state, "processing", True),
-        disabled=st.session_state.processing,
-    ):
-        result = process_video(yt_uri)
-        st.session_state.videos.append(result)
-        st.session_state.processing = False
-        st.rerun()
-
-    st.header("Processed videos:")
-    for video in st.session_state.videos:
-        with st.container(border=True):
-            st.title(video["title"])
-            st.image(video["thumbnail"], width=320)
-            if st.button("Download transcription"):
-                with open(
-                    st.session_state.tempfolder / f"{video['video_id']}.txt"
-                ) as transcript_file:
-                    st.download_button(
-                        f"Download transcription {video['video_id']}",
-                        transcript_file,
-                        file_name=video["video_id"],
-                    )
+    parser = argparse.ArgumentParser(description="Process a local MP4 video file.")
+    parser.add_argument("video_path", type=str, help="Path to the local MP4 file")
+    args = parser.parse_args()
+    
+    
+    result = process_video(args.video_path)
+    print(f"Processing complete: {result}")
 
 if __name__ == "__main__":
     main()
